d78cdc70f58cccaf841249d29270ffd31d87a020
sannith <sannithnalluri2003@gmail.com>
2023-09-14 22:19:17 +0530

    useruiupdate

--- src/Compoents/HOMEPAGE/Faq.js
+++ src/Compoents/HOMEPAGE/Faq.js
@@ -1,50 +1,60 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './Faq.css';
-import {AiOutlinePlus} from 'react-icons/ai'
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
-const Faq = () => {
-    const faqData = [
-        {
-          question: 'What is React?',
-          answer: 'React is a JavaScript library for building user interfaces.'
-        },
-        {
-          question: 'How do I install React?',
-          answer: 'You can install React using npm or yarn. Run "npm install react" or "yarn add react" to get started.'
-        },
-        {
-          question: 'What is React?',
-          answer: 'React is a JavaScript library for building user interfaces.'
-        },
-        {
-          question: 'How do I install React?',
-          answer: 'You can install React using npm or yarn. Run "npm install react" or "yarn add react" to get started.'
-        },
-        // Add more FAQ items as needed
-      ];
-      const [activeIndex, setActiveIndex] = useState(null);
+const FaqPage = () => {
+  const [expandedQuestion, setExpandedQuestion] = useState(null);
+ 
+ 
 
-      const toggleAnswer = (index) => {
-        setActiveIndex(activeIndex === index ? null : index);
-      };
-    
-      return (
-        <div>
- <div className="faq">
-          <h2>Frequently Asked Questions</h2>
-          <ul>
-            {faqData.map((faq, index) => (
-              <li key={index}>
-                <strong onClick={() => toggleAnswer(index)}>{faq.question} <AiOutlinePlus/></strong>
-                <hr/>
-                {activeIndex === index && <p>{faq.answer}</p>}
-              </li>
-            ))}
-          </ul>
-        </div>
-        </div>
-       
-      );
+  const toggleQuestion = (index) => {
+    if (expandedQuestion === index) {
+      setExpandedQuestion(null);
+    } else {
+      setExpandedQuestion(index);
+    }
+  };
+
+  const faqData = [
+    {
+      question: "How does HyperRide's bike rental service work?",
+      answer: "HyperRide's bike rental service allows you to rent a bike for a specified duration. You can choose a bike from our inventory, book it online or in person, and enjoy your ride. We offer flexible rental periods to suit your needs.",
+    },
+    {
+      question: 'What types of bikes does HyperRide offer for rent?',
+      answer: "HyperRide offers a variety of bikes for rent, including mountain bikes, road bikes, city bikes, and electric bikes (e-bikes). Whether you need a bike for off-road adventures or urban commuting, we have options to fit your preferences.",
+    },
+    {
+      question: 'What is the rental pricing and duration at HyperRide?',
+      answer: "HyperRide's rental pricing depends on the type of bike and the duration of the rental. We offer hourly, daily, and weekly rental options, and our pricing is competitive. You can check our website or visit our rental location for specific pricing details.",
+    },
+    {
+      question: 'Does HyperRide provide safety equipment with bike rentals?',
+      answer: 'Yes, safety is important to us at HyperRide. We provide safety equipment such as helmets, reflective gear, and bike locks with every rental. Your safety is our priority, and we encourage all riders to use the provided safety gear.',
+    },
+    {
+      question: 'What is HyperRide\'s cancellation policy?',
+      answer: "HyperRide has a flexible cancellation policy. If you need to cancel your reservation, please notify us in advance. We'll provide you with details on our cancellation policy when you make a reservation.",
+    },
+  ];
+  
+
+  return (
+    <div className='faq-container'>
+      <h1>Frequently Asked Questions</h1>
+      <div className='faq-list'>
+        {faqData.map((faq, index) => (
+          <div className='faq-item' key={index}>
+            <div className='faq-question' onClick={() => toggleQuestion(index)}>
+              <span>{faq.question}</span>
+              {expandedQuestion === index ? <AiOutlineMinus /> : <AiOutlinePlus />}
+            </div>
+            {expandedQuestion === index && <div className='faq-answer'>{faq.answer}</div>}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Faq;
+export default FaqPage;
